d4b4a4e6e92380bf3290f7e07a8f72e2aa641913
Ali Akbar Khan <aliakbarkhana79@gmail.com>
2026-05-27 18:49:16 +0530

    working with variables

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
+    // this whole code is writtin from terminal using Vim. (super cool)
     let mut todos: Vec<String> = Vec::new();
-
+    /*
     todos.push(String::from("Learn Rust"));
     todos.push(String::from("Build a project"));
     todos.push(String::from("Push to Github"));
-
     println!("my tools");
     for (i, todo) in todos.iter().enumerate(){
         println!(" {}. {}", i+1, todo);
-    }
+    }*/
+
+    let mut x = 10;
+    x = 5;
+    println!("value of x= {}",x);
+
 }
