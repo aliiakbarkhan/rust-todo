c25b8c0c71efae33e8202cd2b920714dfc3e7b21
Ali Akbar Khan <aliakbarkhana79@gmail.com>
2026-05-27 14:03:47 +0530

    feat: print hardcoded todo list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut todos: Vec<String> = Vec::new();
+
+    todos.push(String::from("Learn Rust"));
+    todos.push(String::from("Build a project"));
+    todos.push(String::from("Push to Github"));
+
+    println!("my tools");
+    for (i, todo) in todos.iter().enumerate(){
+        println!(" {}. {}", i+1, todo);
+    }
 }
